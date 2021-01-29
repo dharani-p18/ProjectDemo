15df9438088a9299b51a7bd96eb6c530b90a3e15
dharani-p18 <78131209+dharani-p18@users.noreply.github.com>
2021-01-29 08:27:05 +0530

    Update Demo2.java

--- MyProject/src/com/cg/demo/Demo2.java
+++ MyProject/src/com/cg/demo/Demo2.java
@@ -4,7 +4,9 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("second class");
+	System.out.println("second class");
+	//this line is edited from github
+	System.out.println("from github");
 	}
 
 }
